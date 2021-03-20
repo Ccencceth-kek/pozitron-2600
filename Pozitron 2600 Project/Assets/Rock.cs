f87e2cbc69ff14f2be0801f7557fa073d75e2fe3
Ccencceth <ccencceth@gmail.com>
2021-03-20 15:34:11 +0800

    Spinning Rock
    rock spin

--- Pozitron 2600 Project/Assets/Rock.cs
+++ Pozitron 2600 Project/Assets/Rock.cs
@@ -7,9 +7,15 @@ public class Rock : MonoBehaviour
     public enum RockType { Big, Medium, Small };
     public RockType rockType;                       // list rock types
 
+    [SerializeField] float maxRotSpeed = 50.0f;
+    [SerializeField] float minRotSpeed = 10.0f;
+
+    private float rotationSpeed = 0.0f;
+    private float rotation = 0.0f;
+
     void Awake()
     {
-        switch(rockType)
+        switch(rockType) // scale rock according to size
         {
             case RockType.Big:
                 transform.localScale = new Vector3(1.5f, 1.5f, 1.0f);
@@ -22,11 +28,18 @@ public class Rock : MonoBehaviour
                 break;
         }
 
+        rotationSpeed = Random.Range(minRotSpeed, maxRotSpeed);
+
+        if (Random.Range(1, 3) == 1)
+        {
+            rotationSpeed *= -1;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rotation += rotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotation);
     }
 }
